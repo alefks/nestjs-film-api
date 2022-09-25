7feadbeac1e99b10ebab79d859675dc3f0811e1d
Alessandro Kobashigawa <ale_cheque_mate@hotmail.com>
2022-09-25 08:11:22 -0300

    refactor(auth.controller): add FileInterceptor to auth login route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  @UseGuards(AuthGuard('local'))
+
   @Post('login')
-  async login(@Req() req: any) {
-    return await this.authService.login(req);
+  @UseInterceptors(FileInterceptor(''))
+  async login(@Body() data: LoginDto) {
+    return await this.authService.login(data);
   }
 }
