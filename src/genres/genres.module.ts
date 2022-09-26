42423c7ab0d745418063ddba2ebb6d1569e36298
Alessandro Kobashigawa <ale_cheque_mate@hotmail.com>
2022-09-26 03:18:27 -0300

    refactor(genres.module): import Genre and Film entity

--- src/genres/genres.module.ts
+++ src/genres/genres.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GenresService } from './genres.service';
 import { GenresController } from './genres.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Genre } from './entities/genre.entity';
+import { Film } from '../films/entities/film.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Genre, Film])],
   controllers: [GenresController],
   providers: [GenresService]
 })
