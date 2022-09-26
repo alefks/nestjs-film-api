84f0576730371975646c21ea84c5c24f3efb80c0
Alessandro Kobashigawa <ale_cheque_mate@hotmail.com>
2022-09-26 03:22:49 -0300

    create-film.dto: create dto for inserting new film

--- src/films/dto/create-film.dto.ts
+++ src/films/dto/create-film.dto.ts
@@ -1,1 +1,22 @@
-export class CreateFilmDto {}
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateFilmDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  cover: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  releaseYear: number;
+
+  @IsOptional()
+  @IsNumber(
+    {},
+    { each: true, message: 'Genres needs to be an array of numbers.' },
+  )
+  genres: number[];
+}
