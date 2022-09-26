f099be763c46180e7022c5a19d5fa41a635270bc
Alessandro Kobashigawa <ale_cheque_mate@hotmail.com>
2022-09-26 03:25:09 -0300

    create-genre.dto: create dto for film genre creation

--- src/genres/dto/create-genre.dto.ts
+++ src/genres/dto/create-genre.dto.ts
@@ -1,1 +1,12 @@
-export class CreateGenreDto {}
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { Film } from '../../films/entities/film.entity';
+
+export class CreateGenreDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsNumber({}, { each: true, message: 'Films must be an array of numbers.' })
+  films: Film[];
+}
