f040a0c195d70f9947a09059da295a7c20a393db
Alessandro Kobashigawa <ale_cheque_mate@hotmail.com>
2022-09-26 11:47:38 -0300

    swagger: add api doc to app

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +8,15 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+  
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs Film API')
+    .setDescription('')
+    .setVersion('1.0')
+    .addTag('films')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
