3ee2b413959ebd217f6513ab1840dc079c1b9e5a
Alessandro Kobashigawa <ale_cheque_mate@hotmail.com>
2022-10-01 15:12:42 -0300

    refactor(messages.helper): add user not found message helper

--- src/helpers/messages.helper.ts
+++ src/helpers/messages.helper.ts
@@ -2,4 +2,5 @@ export const MessagesHelper = {
   PASSWORD_INVALID:
     'The password must be 8 characters long, must contain uppercase and lowercase characters, numbers and special characters.',
   PASSWORD_OR_USERNAME_INVALID: 'Username and/or password are invalid.',
+  USER_NOT_FOUND: 'User not found',
 };
